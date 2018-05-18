7dc46e4911ec426da6015c60749344b68641e65d
Ankit-Gupta-1511 <ankit.gupta.cs1511@gmail.com>
2018-05-19 00:55:51 +0530

    hero image part done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,49 @@
 $(document).ready(function(){
     $("#chat-btn").click(function(){
 
-        var responseContainer = document.getElementsByClassName("response")[0];
+        // var responseContainer = document.getElementsByClassName("response")[0];
+        var chatScreen = document.getElementById('chat-screen');
+        var inputChatBox = document.createElement('div');
+        inputChatBox.className = "chat-box";
+        var responseChatBox = document.createElement('div');
+        responseChatBox.className = "chat-box";
 
+        // creating new response container
+        var responseContainer = document.createElement('div');
+        var p = document.createElement('p');
+        var responseImage = document.createElement('img');
+
+        responseImage.src = "images/my-chat.jpg";
+        responseImage.className = "chat-image";
+
+        responseContainer.className = "chat-msg-container response-container";
+        p.className = "response";
+
+        
+        // getting the input msg
         var input = document.getElementById("chat-input").value;
+
+        //putting msg in a container
+        var inputContainer = document.createElement('div');
+        var pInput = document.createElement('p');
+        var inputImage = document.createElement('img');
+
+        inputImage.src = "images/my-chat1.jpg";
+        inputImage.className = "chat-image";
+
+        inputContainer.className = "chat-msg-container input-container";
+        pInput.className = "input";
+        // adding value in container
+        pInput.textContent = input;
+        
+        //appending container
+        inputContainer.appendChild(pInput);
+        
+        inputChatBox.appendChild(inputContainer);
+        inputChatBox.appendChild(inputImage);
+
+        chatScreen.appendChild(inputChatBox);
+        
 
         var data = {
             "msg": input
@@ -13,7 +53,12 @@ $(document).ready(function(){
 
         
         $.post("https://chat-web-app-ankit.herokuapp.com/chat/get/response", data).done(function(response){
-            responseContainer.textContent = response.response;
+            p.textContent = response.response;
+            responseContainer.appendChild(p);
+            
+            responseChatBox.appendChild(responseImage);
+            responseChatBox.appendChild(responseContainer);
+            chatScreen.appendChild(responseChatBox);
         });
 
      
