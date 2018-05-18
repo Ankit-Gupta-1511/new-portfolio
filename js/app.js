0654a364c809f9b6ecfda47e659729a04cb055b2
Ankit-Gupta-1511 <ankit.gupta.cs1511@gmail.com>
2018-05-18 19:38:14 +0530

    added chatbot integration

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#chat-btn").click(function(){
+
+        var responseContainer = document.getElementsByClassName("response")[0];
+
+        var input = document.getElementById("chat-input").value;
+
+        var data = {
+            "msg": input
+        };
+
+        $.post("https://chat-web-app-ankit.herokuapp.com/chat/get/response", data).done(function(response){
+            responseContainer.textContent = response;
+        });
+    });
+});
